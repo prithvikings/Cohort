5a016487ad4bb7c9e8798893eea2f84c6c73c45b
Prithvi Raj <prithvi07raj07@gmail,com>
2024-07-16 18:01:28 +0530

    function

--- CLASS-1/complexprimitive/third.js
+++ CLASS-1/complexprimitive/third.js
@@ -1,2 +1,22 @@
 //write a program that prints all the male people first name given a complex object
+const user=[{
+    name:"John",
+    gender:"male",
+    age:20,
+},
+{
+    name:"Jane",
+    gender:"female",
+    age:21,
+},
+{
+    name:"Jack",
+    gender:"male",
+    age:22,
+}]
 
+for(var i=0;i<user.length;i++){
+    if(user[i].gender=="male"){
+        console.log(user[i].name);
+    }
+}
