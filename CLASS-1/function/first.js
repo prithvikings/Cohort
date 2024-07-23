fb0d58ecc519ef350c9df4ca64673c47210086b3
Prithvi Raj <prithvi07raj07@gmail,com>
2024-07-23 22:45:26 +0530

    .

--- CLASS-1/function/first.js
+++ CLASS-1/function/first.js
@@ -7,11 +7,11 @@ function sum(){
 }
 console.log(sum());
 //write another function that displays this result in a preety format
-const sum=()=>{
-    let a=parseInt(prompt("enter the first number"));
-    let b=parseInt(prompt("enter the second number"));
-    let c=a+b;
-    return c;
-}
-console.log(sum());
+// const sum=()=>{
+//     let a=parseInt(prompt("enter the first number"));
+//     let b=parseInt(prompt("enter the second number"));
+//     let c=a+b;
+//     return c;
+// }
+// console.log(sum());
 //write another function that takes this sum and prints it in passive tense
